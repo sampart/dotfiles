4439a05c515a327e31b8db7e81f9784134a0ef2b
Sam Partington <sampart@github.com>
2020-05-18 14:55:07 +0100

    Delete mac-specific setup file; normal one works now

--- mac-setup.sh
+++
@@ -1,7 +0,0 @@
-DOTFILES_ROOT="`pwd`"
-
-rm -f ~/.gitconfig
-ln -s $DOTFILES_ROOT/more-home-dir-config/.gitconfig ~/.gitconfig
-rm -f ~/.gitignore
-ln -s $DOTFILES_ROOT/more-home-dir-config/.gitignore ~/.gitignore
-./scripts-in-path/setup.sh
