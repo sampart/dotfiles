9dfa971985a295c42907d88f0a74ba3a4077efcf
Sam Partington <sampart@github.com>
2020-05-18 16:04:22 +0100

    Make scripts work when run from different dir

--- setup.sh
+++ setup.sh
@@ -10,7 +10,9 @@
 echo "\033[32mMain dotfiles install script started..."
 echo "\033[0m"
 
-DOTFILES_ROOT="$(pwd)"
+# The full path to the directory containing this script
+# https://unix.stackexchange.com/a/76518
+DOTFILES_ROOT=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; /usr/bin/pwd || /bin/pwd || pwd)
 
 # ===================
 # Helper functions
@@ -45,7 +47,7 @@ for source in $(find $DOTFILES_ROOT/zshrc -name \*.symlink)
 do
   dest="$HOME/.zshrc-includes/$(basename \"${source%.*}\")"
 
-  rm -rf "$dest"
+  rm -f "$dest"
   ln -s "$source" "$dest"
   printf "symlinked $source to $dest\n"
 done
@@ -58,22 +60,20 @@ printf '\n'
 
 echo "Other home dir config files..."
 
-cd more-home-dir-config
+OTHER_CONFIG=$DOTFILES_ROOT/more-home-dir-config
 
 # copy over the right OS-specific git config
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  cp os-gitconfig/.gitconfig_mac ./.gitconfig_os
+  cp "$OTHER_CONFIG"/os-gitconfig/.gitconfig_mac "$OTHER_CONFIG"/.gitconfig_os
 else
-  cp os-gitconfig/.gitconfig_linux ./.gitconfig_os
+  cp "$OTHER_CONFIG"/os-gitconfig/.gitconfig_linux "$OTHER_CONFIG"/.gitconfig_os
 fi
 
 # first delete the existing versions of these files in home
-find . -maxdepth 1 -type f -exec rm -f $HOME/{} \;
+find "$OTHER_CONFIG" -maxdepth 1 -type f -execdir rm -f $HOME/{} \;
 
 # now symlink the new ones
-find . -maxdepth 1 -type f -exec ln -s $DOTFILES_ROOT/more-home-dir-config/{} $HOME/{} \;
-
-cd $DOTFILES_ROOT
+find "$OTHER_CONFIG" -maxdepth 1 -type f -execdir ln -s "$OTHER_CONFIG"/{} $HOME/{} \;
 
 printf "Other home dir config files symlinked\n"
 
